81943a3fb49b3887ee739676cdb58f7c0904a721
Sam Landfried <samlandfried@gmail.com>
2018-08-19 12:50:00 -0600

    Add CheatSheet component

---
+++ src/components/CheatSheet.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CheatSheet extends Component {
+  render() {
+    return (
+      <div>New Cheat Sheet!</div>
+    );
+  }
+}
+
+export default CheatSheet;
