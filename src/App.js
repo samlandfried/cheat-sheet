bdcc8ed764f0661eeab4a4889949d07990adc5e6
Sam Landfried <samlandfried@gmail.com>
2018-09-01 17:42:17 -0700

    Add new cheat sheet on app load

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import CheatSheet from 'components/CheatSheet';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sheetId: 0,
+      sheets: [],
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -9,12 +17,45 @@ class App extends Component {
           <h1 className="App-title">Cheat Sheets</h1>
         </header>
         <ul style={{ listStyle: 'none' }}>
-          <li>
-            <CheatSheet />
-          </li>
+          {
+            this.state.sheets.map( sheet => (
+              <li key={sheet.id}>
+                <CheatSheet />
+              </li>
+            ))
+          }
         </ul>
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.newSheet();
+  }
+
+  newSheet() {
+    const nextId = this.state.sheetId + 1;
+
+    if (this.state.sheets.length) {
+      const sheets = [...this.state.sheets];
+
+      sheets.unshift({
+        id: nextId,
+      })
+
+      this.setState({
+        sheetId: nextId,
+        sheets,
+      });
+    }
+    else {
+      this.setState({
+        sheetId: nextId,
+        sheets: [{
+          id: nextId,
+        }],
+      });
+    }
   }
 }
 
